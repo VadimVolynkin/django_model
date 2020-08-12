902b995d87917b709abaf50af547bd97a23cfb2c
Vadim Volynkin Terra <71ooo@mail.ru>
2020-08-12 23:59:08 +0300

    add new types

--- first/models.py
+++ first/models.py
@@ -1,7 +1,12 @@
 import uuid
+import datetime
+from enum import Enum
+
+from django.utils import timezone
 
 from django.db import models
 from django.conf import settings
+
 
 class SmallAutoFieldClass(models.Model):
     # auto_field = models.AutoField(primary_key=True)
@@ -9,7 +14,7 @@ class SmallAutoFieldClass(models.Model):
     # big_auto_field = models.BigAutoField(primary_key=True)
     # uuid_field = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 
-    # NUMBERS TYPE ===========================================================
+    # NUMBER TYPES ===========================================================
     # small_integer_field = models.SmallIntegerField(blank=True, null=True)
     # integer_field = models.IntegerField(blank=True, null=True)
     # big_integer_field = models.BigIntegerField(blank=True, null=True)
@@ -19,29 +24,44 @@ class SmallAutoFieldClass(models.Model):
     # decimal_field = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
     # float_field = models.FloatField(blank=True, null=True)
 
-    # STRINGS TYPE ===========================================================
-    char_field = models.CharField(max_length=200, blank=True)
-    text_field = models.TextField(blank=True)
-    slug_field = models.SlugField(max_length=200, blank=True)
-    email_field = models.EmailField(max_length=200, blank=True)
-    url_field = models.URLField(max_length=200, blank=True)
-    generic_ip_address_field = models.GenericIPAddressField(blank=True, null=True)
-    file_path_field = models.FilePathField(path=settings.FILE_PATH_FIELD_DIRECTORY, blank=True)
+    # STRING TYPES ===========================================================
+    # char_field = models.CharField(max_length=200, blank=True)
+    # text_field = models.TextField(blank=True)
+    # slug_field = models.SlugField(max_length=200, blank=True)
+    # email_field = models.EmailField(max_length=200, blank=True)
+    # url_field = models.URLField(max_length=200, blank=True)
+    # generic_ip_address_field = models.GenericIPAddressField(blank=True, null=True)
+    # file_path_field = models.FilePathField(path=settings.FILE_PATH_FIELD_DIRECTORY, blank=True)
 
+    # DATE and TIME TYPES ====================================================
+    # date_field = models.DateField(auto_now_add=True)
+    # time_field = models.TimeField(auto_now=True)
+    # date_time_field = models.DateTimeField(default=timezone.now())
+    # duration_field = models.DurationField()
 
+    # LOGIC TYPES ============================================================
+    # boolean_filed = models.BooleanField(default=True)
+    # null_boolean_field = models.NullBooleanField(default=False)
 
+    # FILE TYPES =============================================================
+    # file_field = models.FileField(upload_to='uploads/%Y/%m/%d/')
+    # image_field = models.ImageField(upload_to='image/%Y/%m/%d/', height_field=None, width_field=None)
+    # binary_field = models.BinaryField(default=bytes('hello world', 'utf-8'))   # convert string hello world in bytes
 
-
-
-
+    # OTHER TYPES ============================================================
+    integer_choices = models.IntegerChoices('Color', names='RED GREEN BLUE')
+    text_choices = models.TextChoices('Color', names='RED GREEN BLUE')
+    choices = models.Choices('Color', names='RED GREEN BLUE')
+    empty_field = models.Empty()
+    json_field = models.JSONField()
 
 
 
     # НЕТ В ДОКУМЕНТАЦИИ 3.0
-    # choices = models.Choices()
+
     # integer_choices = models.IntegerChoices()
     # text_choices = models.TextChoices()
     # comma_separated_integer_field = models.CommaSeparatedIntegerField()
-    # empty_field = models.Empty()
+    # choices = models.Choices('Color', names='RED GREEN BLUE')
     # ip_adress_field = models.IPAddressField()
     # json_field = models.JSONField()
